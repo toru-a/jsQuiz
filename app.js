1ea1afceee09f6224b4b0746484f172d6a72a8ee
toru-a <148777061+toru-a@users.noreply.github.com>
2023-11-08 16:44:16 +0900

    コメント挿入、見やすく編集

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// スタート画面（問題画面表示）
 const quizStart= {
      question: '準備はいい？まもなくスタート！',
     };
 
+// 各問題（問題・選択肢・答え・正解時コメント・誤時コメント）
 const quiz = [
     {
       question: '人気youtuber兼エンジニアのセイト先生の苗字は？',
@@ -35,12 +37,16 @@ const popUp = window.alert
 let quizIndex = 0;
 let score = 0;
 
+// スタート画面表示（選択肢ボタンは非表示）
 const setUpStart = () => {
      $question.textContent = quizStart.question;
      let setUpIndex = 0;
      $items.style.visibility = 'hidden';
     };
 
+setUpStart();
+
+// 問題セット（初回3秒待ち）（選択肢ボタンは表示）
 const SetUpQuiz = () => {
     $question.textContent = quiz[quizIndex].question;
     let btnIndex = 0;
@@ -51,10 +57,9 @@ const SetUpQuiz = () => {
     $items.style.visibility = 'visible';
   };
 
-setUpStart();
-
 setTimeout(`SetUpQuiz()`,3000);
 
+// 結果画面⇒コンティニュー画面（続ける：再読込　キャンセル：すべて非表示）
 const gameOver = () =>{
 　　　$items.style.visibility = 'hidden';
 　　　$question.style.visibility = 'hidden';
@@ -66,6 +71,7 @@ const gameOver = () =>{
      };
 };
 
+// ボタンにイベント機能付与
 let handlerIndex = 0;
     while (handlerIndex < $buttonLen) {
      $buttons[handlerIndex].addEventListener('click', (e) => {
@@ -74,6 +80,7 @@ let handlerIndex = 0;
     handlerIndex++;
     };
 
+// 正誤判定（コメント表示）⇒次問OR結果画面
 const clickHandler = (e) => {
     let answer = quiz[quizIndex].answers.indexOf(e.target.textContent);
     if( quiz[quizIndex].correct === e.target.textContent){
@@ -92,15 +99,18 @@ const clickHandler = (e) => {
     }
 };
 
-//カウントダウンタイマー
-timeLeft = 3;
+
+//カウントダウンタイマー読み込み後3秒
+let timeLeft = 3;
 const $seconds = document.getElementById("seconds");
-function countdown() {
+const countdown = () => {
 	timeLeft--;
-	$seconds.innerHTML = String( timeLeft );
+	$seconds.textContent =  timeLeft ;
 	if (timeLeft > 0) {
-		setTimeout(countdown, 1000);
+		setTimeout(`countdown()`, 1000);
 	}else{
           $seconds.style.visibility='hidden';}
 };
-setTimeout(countdown, 1000);
+setTimeout(`countdown()`, 1000);
+
+
